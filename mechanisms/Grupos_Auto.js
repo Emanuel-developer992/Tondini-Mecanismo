0eaf0a4e80b3fe0542548fec434d669619b1bf98
Emanuel-developer992 <Orion_Developer@outlook.com>
2021-03-25 23:07:32 -0300

    1.3v

--- mechanisms/Grupos_Auto.js
+++ mechanisms/Grupos_Auto.js
@@ -2,15 +2,15 @@ function resolve(process, colleague) {
 
     var userList = new java.util.ArrayList();
 
-    var cGrupo = DatasetFactory.createConstraint("group1Id", "grupo_1","grupo_1", ConstraintType.SHOULD);
+    var cGrupo = DatasetFactory.createConstraint("mgroupID", "grupo_1","grupo_1", ConstraintType.SHOULD);
 
     var arrayconstraints = new Array(cGrupo);
 
-    var arreyGroup = DatasetFactory.getDataset("dsIndex", null, arrayconstraints, null);
+    var arreyGroup = DatasetFactory.getDataset("dsCliente_Mecanismo", null, arrayconstraints, null);
 
 
 	for(var g = 0; g < arreyGroup.values.length; g++){
-        userList.add(arreyGroup.getValue(g.toString(),"colleague1Id"));
+        userList.add(arreyGroup.getValue(g.toString(),"mcolleagueID"));
 
 
     }
